2255d9eefa8e8845b271a7d1fecdcf1b5624b276
Serpent7776 <serpent7776@gmail.com>
2020-11-06 19:57:19 +0100

    Created moon_scene

--- main.lua
+++ main.lua
@@ -2,7 +2,9 @@ local lf = require("lib/love-frame")
 
 local W, H, W_2, H_2
 local moon_top_h
-local moon_vertices
+local scene
+
+local moon_scene = {}
 
 local function lerp(x, y, a)
 	return y * a + x * (1 - a)
@@ -12,34 +14,63 @@ local function sinc(x, y, a)
 	return lerp(x, y, math.sin(a * math.pi / 2))
 end
 
-local function prepare_data()
+local function table_copy(src)
+	local dst = {}
+	for idx, value in pairs(src) do
+		dst[idx] = value
+	end
+	return dst
+end
+
+local function switch_to(new_scene)
+	if scene and scene.unload then
+		scene:unload()
+	end
+	scene = table_copy(new_scene)
+	scene:load()
+end
+
+moon_scene.prepare_data = function(self)
 	moon_top_h = H * 0.1
-	moon_vertices = {}
+	self.moon_vertices = {}
 	local max_i = 10
 	for i = 0, max_i do
 		local c = max_i / 2
 		local x = (i - c) / c * W_2
 		local y = sinc(0, moon_top_h, i / c)
-		moon_vertices[i * 2 + 1] = x
-		moon_vertices[i * 2 + 2] = y
+		self.moon_vertices[i * 2 + 1] = x
+		self.moon_vertices[i * 2 + 2] = y
 	end
 end
+
+moon_scene.load = function(self)
+	self:prepare_data()
+end
+
+moon_scene.update = function(self, dt)
+end
+
+moon_scene.draw = function(self)
+	love.graphics.translate(W_2, -H)
+	love.graphics.setColor(0.86, 0.86, 0.86)
+	love.graphics.polygon('fill', self.moon_vertices)
+	love.graphics.setColor(0.1, 0, 0.86)
+	love.graphics.rectangle('fill', -10, moon_top_h*0.9, 20, moon_top_h*0.5)
+end
+
 
 lf.init = function()
 	W, H = 800, 600
 	W_2, H_2 = W / 2, H / 2
 	-- viewport origin is at bottom, centre and goes right and up
 	lf.setup_viewport(W, -H)
-	prepare_data()
+	switch_to(moon_scene)
 end
 
 lf.update = function(dt)
+	scene:update()
 end
 
 lf.draw = function()
-	love.graphics.translate(W_2, -H)
-	love.graphics.setColor(0.86, 0.86, 0.86)
-	love.graphics.polygon('fill', moon_vertices)
-	love.graphics.setColor(0.1, 0, 0.86)
-	love.graphics.rectangle('fill', -10, moon_top_h*0.9, 20, moon_top_h*0.5)
+	scene:draw()
 end
