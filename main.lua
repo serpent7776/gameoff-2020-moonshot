e214880b7e527f9c4007b0e1cda588bb2c9d3cb0
Serpent7776 <serpent7776@gmail.com>
2020-11-15 00:02:21 +0100

    Move twice as fast

--- main.lua
+++ main.lua
@@ -294,7 +294,7 @@ launched_scene.reset = function()
 	})
 	launched_scene.rocket.offset_x = launched_scene.rocket.width + 10
 	launched_scene.objects = {}
-	launched_scene.rocket_mover = deferred(0.36329, continue, launched_scene.continue_rocket_movement)
+	launched_scene.rocket_mover = deferred(0.36329 / 2, continue, launched_scene.continue_rocket_movement)
 	launched_scene.spawner = deferred(0.6, continue, launched_scene.spawn_object)
 	launched_scene.run_done = conditional(deferred(1, continue, launched_scene.end_run), launched_scene.run_complete)
 end
